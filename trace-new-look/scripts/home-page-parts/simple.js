b9ccf678163d841c6a7e1041a72368fc1c589a36
doopdon <gannonhuiting@gmail.com>
2021-08-05 22:02:53 -0600

    ok now its much better

--- trace-new-look/scripts/home-page-parts/simple.js
+++ trace-new-look/scripts/home-page-parts/simple.js
@@ -1,68 +1,21 @@
 function homePageSimple(){
 
-    let boxStyle = {
-        background:'red',
-        width:'100%',
-        height:'100%',
-    }
-
-    let beamStyle = {
-        background:'blue',
-        width:'100%',
-        height:'100%',
-    }
-
-    let polStyle = {
-        background:'green',
-        width:'100%',
-        height:'100%',
-    }
-
-    let rowStyle = {
-        width:'100%',
-        height:'100%',
-        background:'grey',
-        display:'flex',
-    }
-
-    let colStyle = {
-        width:'100%',
-        height:'100%',
-        background:'grey',
-        display:'flex',
-        flexDirection:'column'
-    }
-
-    let rowCellStyle = {
-        width:'50%',
-        height:'100%',
-        padding:'5%',
-        border:'1px solid black'
-    }
-
-    let colCellStyle = {
-        width:'100%',
-        height:'50%',
-        padding:'5%',
-        border:'1px solid black'
-    }
-
-
-    let row = (child1,child2) => div({style:rowStyle},[
-        div({style:rowCellStyle},[child1]),
-        div({style:rowCellStyle},[child2]),
-    ])
-    let col = (child1,child2) => div({style:colStyle},[
-        div({style:colCellStyle},[child1]),
-        div({style:colCellStyle},[child2]),
+    let row = (child1,child2) => div({class:'split split-row'},[
+        div({class:'cell row-cell'},[child1]),
+        div({class:'cell row-cell'},[child2]),
     ])
 
-    let box = ()=>div({style:boxStyle},[])
-    let beam = ()=>div({style:beamStyle},[])
-    let pol = ()=>div({style:polStyle},[])
+    let col = (child1,child2) => div({class:'split split-col'},[
+        div({class:'cell col-cell'},[child1]),
+        div({class:'cell col-cell'},[child2]),
+    ])
+
+    let box = ()=>div({class: 'content box'},[])
+    let beam = ()=>div({class: 'content beam'},[])
+    let pol = ()=>div({class: 'content pol'},[])
 
     return displayBox({},[
-        div({style:{width:'300px',height:'150px'}},[boxes()])
+        div({style:{width:'300px',height:'150px'}, class:'simple'},[boxes()])
     ])
 
     function boxes(){
